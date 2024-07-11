20bf177736b0d45245574571e30465165db7f8bc
Sanjaya Gayan <k.m.s.g.rathnayaka@gmail.com>
2024-07-11 19:37:56 +0530

    Update more mobile responsive

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -86,7 +86,7 @@ function Home() {
             />
           </div>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {filteredTasks && filteredTasks.length > 0 ? (
               filteredTasks.map((item) => (
                 <div
